2f3becb04237e6e133316d4ad5ba26b5263d6bc9
maisam2004 <maisam_2004@hotmail.com>
2023-10-17 11:31:52 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function calculateIncorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function inscrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
