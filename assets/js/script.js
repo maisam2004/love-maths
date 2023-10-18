e6e89fc4f5ea5ba674daefebdbb3a9b16e1dfee9
maisam2004 <maisam_2004@hotmail.com>
2023-10-18 12:03:25 +0000

    Modify divition and add math floor

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ function calculateCorrectAnswer(){
         return [operand1 - operand2,"subtract"];
 
     }else if(operator === "/"){
-        return [operand1 / operand2,"division"];
+        return [Math.floor(operand1 / operand2),"division"];
 
     }else{
         alert(`unimplemented operator ${operator}`) ;
@@ -149,8 +149,8 @@ function displayMultiplyQuestion(operand1,operand2){
 }
 
 function displayDivisionQuestion(operand1,operand2){
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = `/`;
 
 }
